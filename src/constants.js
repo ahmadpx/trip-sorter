bc5c401152053e3c76ea1737e4cd56466e4f7904
Ahmed Abdallah <ahmadpx@hotmail.com>
2019-01-29 00:36:10 +0400

    Add constants

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const CHEAPEST = "cheapest";
+export const FASTEST = "fastest";
+export const ORIGIN = "origin";
+export const DESTINATION = "destination";
+export const TRANSPORT_TYPES = {
+  BUS: "bus",
+  TRAIN: "train",
+  CAR: "car"
+};
